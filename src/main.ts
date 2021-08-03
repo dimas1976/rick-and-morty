a7e27fdbb78375db918e047d91239ad40370d447
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-03 14:33:50 +0200

    Override functionality with utility function

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,42 @@
 import './style.css';
+import './utils/createElement';
+import { createElement } from './utils/createElement';
 
-const app = document.querySelector<HTMLDivElement>('#app');
+const container = createElement('div', {
+  className: 'container',
+  childElements: [
+    createElement('header', {
+      className: 'header',
+      childElements: [
+        createElement('h1', {
+          className: 'header__heading',
+          innerText: 'Rick and Morty',
+        }),
+      ],
+    }),
+    createElement('main', {
+      className: 'main',
+      childElements: [
+        createElement('input', {
+          className: 'main__input',
+          placeholder: 'Search for a character',
+        }),
+      ],
+    }),
+  ],
+});
 
-const main = document.createElement('main');
+/*const main = document.createElement('main');
 const header = document.createElement('header');
 const title = document.createElement('h1');
-title.innerText = 'Hallo Rick';
+title.innerText = 'Rick and Morty';
 const input = document.createElement('input');
-input.placeholder = 'Enter Data';
+input.placeholder = 'search for a character';
 
 header.append(title);
-main.append(input);
+main.append(input);*/
+const app = document.querySelector<HTMLDivElement>('#app');
 
 if (app !== null) {
-  app.append(header, main);
+  app.append(container);
 }

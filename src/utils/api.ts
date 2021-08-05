03e568567d2187b3f6bb389efa4902e9c422153e
dimas1976 <kogan.dmitriy@gmail.com>
2021-08-05 15:41:32 +0200

    Format characters

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,18 @@
 import '../types';
-export async function getCharacters(): Promise<[]> {
+import { AllCharactersFromAPI, Character, CharacterFromAPI } from '../types';
+export async function getCharacters(): Promise<Character[]> {
   const response = await fetch('https://rickandmortyapi.com/api/character');
-  const data = await response.json();
+  const data: AllCharactersFromAPI = await response.json();
   const characters = data.results;
-  console.log(characters);
-  return characters;
+  const formattedCharacters: Character[] = characters.map((character) => {
+    const formattedCharacter = {
+      name: character.name,
+      thumbnail: character.image,
+      status: character.status,
+      species: character.species,
+      origin: character.origin.name,
+    };
+    return formattedCharacter;
+  });
+  return formattedCharacters;
 }
